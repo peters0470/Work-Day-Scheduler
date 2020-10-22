f048b1bed96f0e10e2c5b7283cc1a15cdeeb2e58
unknown <peters0470@gmail.com>
2020-10-22 15:01:40 -0500

    added row colors function and added class to html

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ var currentDate = moment().format("MMMM Do YYYY");
 var currentHour = moment().format("H");
 var tasksToDo = [];
 var toDoRow = $(".row");
+var taskArea = $(".container");
 
 function beginScheduler() {
     toDoRow.each(function() {
     var hourRow = $(this);
-    var toDoRowHour = parseInt(hourRow.attr("hour"));
+    var toDoRowHour = parseInt(hourRow.attr("data-hour"));
 
     var toDoObj = {
         hour: toDoRowHour,
@@ -21,12 +22,31 @@ function beginScheduler() {
 currentDay.text(currentDate);
 
 function saveTask() {
-    var tasksToDoHour = $(this).parent().attr("hour");
+    var tasksToDoHour = $(this).parent().attr("data-hour");
     var taskAdd = (($(this).parent()).children("textarea")).val();
     for (var i = 0 ; i < tasksToDo.length; i++) {
         if (tasksToDo[i].hour == tasksToDoHour) {
             tasksToDo[i].text = taskAdd;
         }
     }
-    localStorage.setItem("tasks", JSON.stringify(tasksToDo));
+    localStorage.setItem("tasks", JSON.stringify(tasksToDo));   
 }
+
+function colorRows() {
+    toDoRow.each(function() {
+        var eachRow = $(this);
+        var eachRowHour = parseInt(eachRow.attr("data-hour"));
+        if (eachRowHour == currentHour) {
+            eachRow.addClass("present").removeClass("future past");
+    }
+        if (eachRowHour > currentHour) {
+            eachRow.addClass("future").removeClass("present past");
+        }
+        if (eachRowHour < currentHour) {
+            eachRow.addClass("past").removeClass("present future");
+        }
+    });
+}
+
+taskArea.on("click", "button", saveTask);
+colorRows();
